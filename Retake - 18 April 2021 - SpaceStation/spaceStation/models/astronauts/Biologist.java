c85f57c0c1ba931c537b03a2c20b65aeec23dc43
Boris Boev <83350776+borisboev79@users.noreply.github.com>
2022-08-29 00:08:04 +0300

    Add files via upload

---
+++ Retake - 18 April 2021 - SpaceStation/spaceStation/models/astronauts/Biologist.java
@@ -0,0 +1,16 @@
+package spaceStation.models.astronauts;
+
+public class Biologist extends BaseAstronaut{
+
+    public Biologist(String name) {
+        super(name, 70);
+    }
+    @Override
+    public void breath() {
+        setOxygen(getOxygen() - 5);
+        if(getOxygen() < 0) {
+            setOxygen(0);
+        }
+    }
+
+}
